b6b9932218886f1c8662cb7df7583aecba0bcf56
Miljan Puletic <miljan.puletic@seavus.com>
2022-09-05 13:17:10 +0200

    update

--- 7Ch/Crane.java
+++ 7Ch/Crane.java
@@ -1,10 +1,15 @@
 public record Crane(int numberEggs, String name) {
-
-	public Crane {
-		numberEggs = 999;
-	}
+	 public Crane(String firstName, String lastName) {
+		 this(0, firstName + " " + lastName);
+		
+	 } 
 	 
-	public Crane {
-		numberEggs = 123;
-	}
+	 public Crane() {this(0, "");}
+	 
+	 public Crane{ 
+		 this();
+		 numberEggs = 1; //
+		 name = "";
+		 
+	 }
 }
