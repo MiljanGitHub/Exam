5afb10b101e0895d293b979e61c1f40639583fbc
Miljan Puletic <miljan.puletic@seavus.com>
2022-09-30 18:04:50 +0200

    update

---
+++ 9Ch/Demo1.java
@@ -0,0 +1,7 @@
+public class Demo1 {
+
+    public class LongTailAnimal {
+        protected void chew(List<Object> input) {}
+        protected void chew(List<Double> input) {} // DOES NOT COMPILE
+    }
+}
